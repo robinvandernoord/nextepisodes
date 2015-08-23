9f8214706de1ab8adbd19b38c40ce34251c9f678
Robin van der Noord <robinvandernoord@gmail.com>
2015-08-23 14:18:55 +0200

    changed server.js

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ var SampleApp = function() {
         self.createRoutes();
         self.app = express.createServer();
         // serve static assets
-                self.app.use(express.static(__dirname));
+        self.app.use(express.static(__dirname));
         //  Add handlers for the app (from the routes).
         for (var r in self.routes) {
             self.app.get(r, self.routes[r]);
